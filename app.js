ef615a67efd6b513c934dcd740658a5ca510d291
Perry Picker <hugolanderos7@outlook.com>
2019-12-21 14:58:58 -0600

    Cleaning the project

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+const GoogleStrategy = require("passport-google-oauth").OAuthStrategy;
 const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
@@ -36,7 +36,6 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.plugin(passportLocalMongoose);
-userSchema.plugin(findOrCreate);
 
 // mongoose models
 const User = new mongoose.model("User", userSchema);
@@ -46,25 +45,9 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
-    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
-},
-    (accessToken, refreshToken, profile, done) => {
-        User.findOrCreate({ googleId: profile.id }, (err, user) => {
-            return done(err, user);
-        });
-    }
-));
-
 app.get("/", (req, res) => {
     res.render("home");
 });
-
-app.get("/auth/google", passport.authenticate("google", { scope: ["https://www.googleapis.com/auth/plus.login"] }));
-
 
 app.get("/register", (req, res) => {
     res.render("register");
@@ -85,7 +68,7 @@ app.get("/secrets", (req, res) => {
 app.get("/logout", (req, res) => {
     req.logout();
     res.redirect("/");
-});
+  });
 
 app.post("/register", (req, res) => {
 
